2c749cd45f8bf1933ca64a703ae7cd4be085913c
LeonSavi <leonardo.saviane@gmail.com>
2025-08-07 17:30:37 +0200

    Added text cleaner

---
+++ scripts/cleaner.py
@@ -0,0 +1,34 @@
+import shutil
+import language_tool_python
+
+
+class Text_Cleaner():
+
+    def __init__(self,
+                 txt_lang:str):
+        if not self.is_java_installed():
+            self.tool = None # LanguageToolPublicAPI does not work
+            raise Warning("Please install java to use language-tool-python")
+        else:
+            self.tool = language_tool_python.LanguageTool(language=txt_lang)
+    
+    def _language_tool(self,text:str):
+        return language_tool_python.utils.correct(
+            text,
+            self.tool.check(text))
+    
+    def clean_up(self,text:str|list[str]) -> str|list[str]:
+        if isinstance(text,list):
+            return list(map(self._language_tool,text))
+        elif isinstance(text,str):
+            return self._language_tool(text)
+        else:
+            return text #maybe a generator ?
+    
+    @staticmethod
+    def is_java_installed() -> bool:
+        return shutil.which("java") is not None
+        
+    
+    
+    
