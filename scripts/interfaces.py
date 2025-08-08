f6f96da57e09f9cb41bcc99fb6eec94928967711
GBegg <beggio.guido@gmail.com>
2025-08-08 15:50:13 +0200

    add alternative approach
    simpler (only functions and basic classes)
    
    does not collide with any other file, just a reference alternative
    implementation
    
    to be tested

--- scripts/interfaces.py
+++ scripts/interfaces.py
@@ -72,14 +72,19 @@ class Box(NamedTuple):
             Point(max_x, max_y)
         )
     
-    def defaultBox() -> Self:
-        return Box(
+    @classmethod
+    def defaultBox(cls) -> Self:
+        return cls(
             bottom_left=Point(0, 0),
             bottom_rigth=Point(0, 0),
             top_left=Point(0, 0),
             top_right=Point(0, 0)
         )
         
+
+
+
+
 
 
 class ReadText(NamedTuple):
