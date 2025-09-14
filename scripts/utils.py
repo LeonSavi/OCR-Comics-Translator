bec6fb526d934d87cff4be3b4e64783cca288e93
LeonSavi <leonardo.saviane@gmail.com>
2025-09-14 22:41:26 +0200

    started code

---
+++ scripts/utils.py
@@ -0,0 +1,8 @@
+class LoopingTime:
+    def __init__(self, timeout: int):
+
+        # TO DO: if loop longer than sec threshold skip.
+
+    def iterate(self,iterable, function):
+
+        return None
